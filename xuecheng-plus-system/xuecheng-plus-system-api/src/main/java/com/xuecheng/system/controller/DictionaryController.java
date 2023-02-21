b2d05988c2cd504f16f49e1a0d71d7e317cff917
hope4cc <1602774287@qq.com>
2023-02-21 18:17:26 +0800

    媒资工程搭建完毕！

--- xuecheng-plus-system/xuecheng-plus-system-api/src/main/java/com/xuecheng/system/controller/DictionaryController.java
+++ xuecheng-plus-system/xuecheng-plus-system-api/src/main/java/com/xuecheng/system/controller/DictionaryController.java
@@ -4,6 +4,7 @@ import com.xuecheng.system.model.po.Dictionary;
 import com.xuecheng.system.service.DictionaryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ import java.util.List;
  * @author itcast
  */
 @Slf4j
+@CrossOrigin
 @RestController
 public class DictionaryController  {
 
